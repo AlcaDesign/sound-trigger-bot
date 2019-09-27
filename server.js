563fe5a2c2eeabd39dc959149c99faaad1006243
Jacob Foster <jfost3@gmail.com>
2019-09-27 03:58:09 -0500

    Move getSoundEffects and add state for TTS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const socketIO = require('socket.io');
 
+const { getSoundEffects, state } = require('./lib/config');
 const events = require('./lib/events');
 const chat = require('./lib/chat-client');
 
@@ -12,6 +13,21 @@ const server = app.listen(process.env.HTTP_PORT, () => {
 const io = socketIO(server);
 
 app.use(express.static('www', { extensions: [ 'html' ] }));
+
+
+app.get('/api/sounds/list', async (req, res) => {
+	const sounds = await getSoundEffects();
+	res.json({ sounds });
+});
+
+app.get('/api/state', (req, res) => {
+	res.json(state);
+});
+
+app.post('/api/state/tts/toggle', (req, res) => {
+	state.tts.enabled = !state.tts.enabled;
+	res.json(state);
+});
 
 app.use((req, res, next) => {
 	res.sendStatus(404);
