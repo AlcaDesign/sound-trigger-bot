2da07a7d114a326d76dfe946ba583285947adaf4
Jacob Foster <jfost3@gmail.com>
2019-09-27 03:59:05 -0500

    Add control panel buttons for toggling TTS

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -1,1 +1,47 @@
-// UI code
+const apiBase = location.origin;
+
+let state = {};
+
+// /**
+//  * @prop {Element} toggleTTS
+//  */
+const elements = {
+	/** @type {Element} */
+	toggleTTS: document.getElementById('toggle-tts')
+};
+
+elements.toggleTTS.addEventListener('click', () => {
+	ttsToggle();
+});
+
+getState().then(updateState);
+
+function update() {
+	elements.toggleTTS.querySelector('span').textContent =
+		state.tts.enabled ? '✅' : '❌';
+}
+
+function updateState(_state) {
+	state = _state;
+	update();
+}
+
+function request({ url = '', qs = {}, method = 'get', headers = {} } = {}) {
+	url += new URLSearchParams(qs);
+	return fetch(url, { headers, method });
+}
+
+function api({ endpoint = '', qs, method }) {
+	const url = `${apiBase}/api/${endpoint}`;
+	return request({ url, qs, method })
+	.then(res => res.json());
+}
+
+function getState() {
+	return api({ endpoint: 'state' });
+}
+
+function ttsToggle() {
+	return api({ endpoint: 'state/tts/toggle', method: 'post' })
+		.then(updateState);
+}
